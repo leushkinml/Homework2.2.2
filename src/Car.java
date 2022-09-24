50ba1f17cefba2c5430da05135cdf002299d1944
leush <leushkinml@mail.ru>
2022-09-25 00:14:30 +0300

    ДЗ 2: 2-е задание выполнено

---
+++ src/Car.java
@@ -0,0 +1,98 @@
+import java.util.Objects;
+
+public class Car {
+
+    // ДЗ 1: 3-е задание
+    /*
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int productionYear;
+    String productionCountry;
+    */
+
+    // ДЗ 2: 2-е задание
+    private final String brand;
+    private final String model;
+    private double engineVolume;
+    private String color;
+    private int productionYear;
+    private String productionCountry;
+
+    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public void setProductionCountry(String productionCountry) {
+        this.productionCountry = productionCountry;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car)) return false;
+        Car car = (Car) o;
+        return getEngineVolume() == car.getEngineVolume() && getProductionYear() == car.getProductionYear() &&
+                getBrand().equals(car.getBrand()) && getModel().equals(car.getModel()) &&
+                getColor().equals(car.getColor()) && getProductionCountry().equals(car.getProductionCountry());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBrand(), getModel(), getEngineVolume(), getColor(),
+                getProductionYear(), getProductionCountry());
+    }
+
+    @Override
+    public String toString() {
+        return "Автомобиль марки: '" + brand + '\'' +
+                ", Модель: '" + model + '\'' +
+                ", Объём двигателя в литрах: " + engineVolume +
+                ", Цвет: '" + color + '\'' +
+                ", Год производства: " + productionYear +
+                ", Страна сборки: '" + productionCountry + '\'' +
+                '.';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,76 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // ДЗ 1: 3-е задание
+
+        /*
+        Car lada = new Car();
+        lada.brand = "Lada";
+        lada.model = "Granta";
+        lada.engineVolume = 1.7;
+        lada.color = "жёлтый";
+        lada.productionYear = 2015;
+        lada.productionCountry = "Россия";
+
+        Car audi = new Car();
+        audi.brand = "Audi";
+        audi.model = "A8 50 L TDI quattro";
+        audi.engineVolume = 3.0;
+        audi.color = "чёрный";
+        audi.productionYear = 2020;
+        audi.productionCountry = "Германия";
+
+        Car bmw = new Car();
+        bmw.brand = "BMW";
+        bmw.model = "Z8";
+        bmw.engineVolume = 3.0;
+        bmw.color = "чёрный";
+        bmw.productionYear = 2021;
+        bmw.productionCountry = "Германия";
+
+        Car kia = new Car();
+        kia.brand = "Kia";
+        kia.model = "Sportage";
+        kia.engineVolume = 2.4;
+        kia.color = "красный";
+        kia.productionYear = 2018;
+        kia.productionCountry = "Южная Корея";
+
+        Car hyundai = new Car();
+        hyundai.brand = "Hyundai";
+        hyundai.model = "Avante";
+        hyundai.engineVolume = 1.6;
+        hyundai.color = "оранжевый";
+        hyundai.productionYear = 2016;
+        hyundai.productionCountry = "Южная Корея";
+
+        presentingCar(lada);
+        presentingCar(audi);
+        presentingCar(bmw);
+        presentingCar(kia);
+        presentingCar(hyundai);*/
+
+
+        // ДЗ 2: 2-е задание
+
+        Car lada = new Car("Lada", "Granta", 1.7, "жёлтый", 2015, "Россия");
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "чёрный", 2020, "Германия");
+        Car bmw = new Car("BMW", "Z8", 3.0, "чёрный", 2021, "Германия");
+        Car kia = new Car("Kia", "Sportage", 2.4, "красный", 2018, "Южная Корея");
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+
+
+        System.out.println(lada);
+        System.out.println(audi);
+        System.out.println(bmw);
+        System.out.println(kia);
+        System.out.println(hyundai);
     }
+
+    // ДЗ 1: 3-е задание
+    /*public static void presentingCar(Car car) {
+        System.out.println("Марка: " + car.brand + ". Модель: " + car.model +
+                ". Объём двигателя в литрах: " + car.engineVolume + ". Цвет: " + car.color +
+                ". Год производства: " + car.productionYear + ". Страна сборки: " + car.productionCountry);
+    }*/
 }
